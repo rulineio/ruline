32f5e5ecd5354fe516277c769f751d4db4014a5b
Ezekiel Grosfeld <eze@ruline.io>
2024-09-22 18:19:13 -0300

    feat: UI Scaffolding & Docker (#14)
    * feat: create ui scaffolding
    
    * feat: rust admin api serving static ui
    
    * feat: add docker for ruline admin

---
+++ ui/rsbuild.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { TanStackRouterRspack } from '@tanstack/router-plugin/rspack';
+
+export default defineConfig({
+    plugins: [pluginReact()],
+    tools: {
+        rspack: {
+            plugins: [TanStackRouterRspack()],
+        },
+    },
+    dev: {
+        writeToDisk: true,
+    },
+    html: {
+        title: 'Ruline',
+    },
+});
